20975ac6f0e01de2bd67f371ee38e057154d517c
moss <dxp157@sohu.com>
2020-02-17 20:37:48 +0800

    增加 bind 的 es3 的兼容写法

--- src/bind.ts
+++ src/bind.ts
@@ -3,7 +3,26 @@ if (!Function.prototype.bind) {
   Function.prototype.bind = bind
 }
 
-function bind(asThis, ...args) {
+// es3 兼容性更强的语法
+var slice = Array.prototype.slice
+function bind(asThis) {
+  // 取到除了第一个 args 后剩下的参数, 等效于 ...args
+  var args = slice.call(arguments, 1)
+  // this 就是函数
+  var fn = this
+  if (typeof fn !== 'function') {// 出错判断
+    throw new Error('必须是函数才能调用 bind')
+  }
+  return function() {
+    // 这里 args2 取全部的参数
+    var args2 = slice.call(arguments, 0)
+    // 参数拼接(这里不能用 call 了只能用 apply，因为 ... 没有了)
+    return fn.apply(asThis, args.concat(args2))
+  }
+}
+
+// es6 新语法代码
+function _bind(asThis, ...args) {
   // this 就是函数
   const fn = this
   return function(...args2) {
